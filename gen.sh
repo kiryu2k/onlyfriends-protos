8c83fa0c3d0468c427636c54a30e0466d8a28b0e
kira kira <sailor.kirill@gmail.com>
2025-08-24 16:48:19 +0300

    add script to run protoc command to gen code from protos

---
+++ gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+services=("auth" "user")
+
+for service in "${services[@]}"; do
+  echo "Generating code for $service.proto..."
+  protoc \
+    --go_out=./$service --go_opt=paths=source_relative \
+    --go-grpc_out=./$service --go-grpc_opt=paths=source_relative \
+    --proto_path=. \
+    --validate_out="lang=go,paths=source_relative:./$service" \
+    "$service.proto"
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to generate for $service.proto"
+    exit 1
+  fi
+done
+
+echo "Generation completed!! ＼(＾▽＾)／"
